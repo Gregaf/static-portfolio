5cd8bd8e2a72a1f185eff90410cfb0913b739c69
Greg Freitas <gregfreitas1997@gmail.com>
2023-03-23 18:37:20 -0400

    Added grid layout for project cards

--- app/(components)/project-card.tsx
+++ app/(components)/project-card.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,7 +9,7 @@ interface TagProps {
 
 function Tag({ children }: TagProps) {
   return (
-    <div className="inline-block shadow-md mx-1 px-4 bg-slate-500 rounded-full">
+    <div className="inline-block shadow-md my-1 mx-1 px-4 bg-slate-500 rounded-full">
       {children}
     </div>
   );
@@ -25,16 +26,25 @@ export default function ProjectCard({ projectData }: ProjectCardProps) {
       target="_blank"
       rel="noopener noreferrer"
     >
-      <div className="group transition py-4 px-5 rounded-xl border-2 border-transparent hover:border-white">
+      <div className="group transition py-4 px-5 rounded-xl bg-zinc-600 border-2 border-transparent hover:bg-teal-600">
         <h2 style={inter.style}>
           {projectData.name} <span>-&gt;</span>
         </h2>
-        <p className="transition m-0 opacity-60 group-hover:opacity-100">
+        <Image
+          className="block mx-auto mt-2 border-2 border-white"
+          src="/placeholder-image.png"
+          alt="project img"
+          width={165}
+          height={165}
+        />
+        <p className="transition m-0 mt-2 opacity-60 group-hover:opacity-100">
           {projectData.description}
         </p>
-        {projectData.tags.map((val, index) => {
-          return <Tag key={index}>{val}</Tag>;
-        })}
+        <div>
+          {projectData.tags.map((val, index) => {
+            return <Tag key={index}>{val}</Tag>;
+          })}
+        </div>
       </div>
     </a>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -19,7 +19,7 @@ export default async function PortfolioBoard() {
 
   return (
     <MainPage>
-      <div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
         {projects.map((project) => {
           return <ProjectCard key={project.ID} projectData={project} />;
         })}
